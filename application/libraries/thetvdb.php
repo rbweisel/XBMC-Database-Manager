860e2ed8080908a3e3321ade18242d8debac8ac8
Vicious <faekie@hotmail.com>
2012-07-31 13:20:23 +0200

    Beginning of TheTVDb API Wrapper

---
+++ application/libraries/thetvdb.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	Class TheTVDb
+	{
+		const _API_URL_ = "http://thetvdb.com/api/";
+		
+		function __construct()
+		{
+			$this->apiKey = "E4FEE841541C44B3";
+		}
+	}
