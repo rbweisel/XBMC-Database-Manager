475c46830ccca0f570846781ec2ed446d679141f
Vicious <faekie@hotmail.com>
2012-07-31 10:55:55 +0200

    Added message displaying when no posters found (No TMDb connection?)

--- application/views/common/posters.php
+++ application/views/common/posters.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="<?php echo base_url();?>css/picker.css" type="text/css" media="screen, projection" />
 <script type="text/javascript" src="<?php echo base_url();?>js/jquery.1.7.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>js/picker.js"></script>
+
 <div id="posterpicker">
 	<div id="current">
 		<?php
@@ -14,17 +15,24 @@
 			{
 				echo "<img id='poster' src='".base_url()."img/na.jpg' />\n\t\t";
 			}
-			echo "<button class='psave' value='".$filename."' id='".$id."'>SAVE</button>\n\t";
-			echo "</div>\n\t<div id='picker'>\n\t\t<div id='posters'>\n";
-			for ($i = 0; $i < count($urls); $i++)
+			if (count($urls) != 0)
 			{
-				$url=$base.$urls[$i]['file_path'];
-				echo "\t\t\t".'<div class="thumbnail">'."\n\t\t\t\t";
-				echo '<img onclick="switchposter(this)" src="'.$url.'" />'."\n\t\t\t";
-				echo '</div>'."\n";
+				echo "<button class='psave' value='".$filename."' id='".$id."'>SAVE</button>\n\t";
+				echo "</div>\n\t<div id='picker'>\n\t\t<div id='posters'>\n";
+				for ($i = 0; $i < count($urls); $i++)
+				{
+					$url=$base.$urls[$i]['file_path'];
+					echo "\t\t\t".'<div class="thumbnail">'."\n\t\t\t\t";
+					echo '<img onclick="switchposter(this)" src="'.$url.'" />'."\n\t\t\t";
+					echo '</div>'."\n";
+				}
+				echo "\t\t\t<div id='baseurl' class='hidden'>".base_url()."</div>\n";
+				echo "\t\t</div>";
 			}
-			echo "\t\t\t<div id='baseurl' class='hidden'>".base_url()."</div>\n";
+			else
+			{
+				echo "<p style='color:white'><strong>NO REMOTE POSTERS FOUND!</strong></p>\n";
+			}
 		?>
-		</div>
 	</div>
 </div>
